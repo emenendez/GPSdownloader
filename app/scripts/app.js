cc13c9e4a6bbb9b8fc73e94bd823f15ed2caa521
Eric Menendez <ericmenendez@gmail.com>
2015-01-13 21:25:01 -0500

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gpsdownloaderApp
+ * @description
+ * # gpsdownloaderApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gpsdownloaderApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gpsdownloaderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gpsdownloaderApp
+ */
+angular.module('gpsdownloaderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
